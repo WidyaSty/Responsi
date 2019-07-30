b9a4abd58388b6942a0f7469e3fe81c45ddb95ee
WidyaSty <48363247+WidyaSty@users.noreply.github.com>
2019-07-30 09:23:50 +0700

    initial commit

---
+++ app/src/main/java/widyasulistyani36/gmail/responsi/Main2Activity.java
@@ -0,0 +1,21 @@
+package widyasulistyani36.gmail.responsi;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+
+public class Main2Activity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
